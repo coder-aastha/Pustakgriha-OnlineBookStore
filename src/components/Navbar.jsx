a53edf19e0ea8399c05d95cbc055816fdfa5ff77
coder-aastha <aasthadahal312@gmail.com>
2023-12-17 20:47:10 +0545

    Commited before pull

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 import { IoSearchOutline } from "react-icons/io5";
-import { MdOutlineLightMode } from "react-icons/md";
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
 import { LuUser } from "react-icons/lu";
 import main_logo from "../images/main_logo.png";
@@ -81,10 +80,6 @@ const Navbar = () => {
 
           <NavLink to="/shopping-cart" className="nav-link">
             <PiShoppingCartSimpleBold />
-          </NavLink>
-
-          <NavLink to="/light-mode" className="nav-link">
-            <MdOutlineLightMode />
           </NavLink>
 
           <NavLink to="/world" className="nav-link">
