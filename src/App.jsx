e8b02e87f6dd0f7b9fdc9128e1b65e2cc113dea8
Misheel31 <misheelrai7@gmail.com>
2023-12-17 15:15:31 +0545

    updated searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./css/App.css";
 //  import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import ImageSlider from "./components/ImageSlider";
 import Genres from "./components/Genres";
 import BestSellers from "./components/BestSellers";
@@ -10,11 +9,6 @@ import NewArrivals from "./components/NewArrivals";
 import Author from "./components/Author";
 import FooterUI from "./components/FooterUI";
 import DeliveryLocationDay from "./components/DeliveryLocationDay";
-
-// import UserLogin from "./components/UserLogin";
-// import UserRegister from "./components/UserRegister";
-// import axios from "axios";
-=======
 import { Home } from "./components/Home";
 import axios from 'axios';
 import {Routes, Route} from'react-router-dom';
@@ -26,11 +20,8 @@ import "../src/css/UserRegister.css";
 import "../src/css/UserLogin.css";
 
 
-
 axios.defaults.baseURL ='http://localhost:3001';
 axios.defaults.withCredentials = true
-
->>>>>>> 6c81dd10d103fb03fafdcd85620d0c4a0f743fd5
 
 function App() {
   return (
