95f7f4719ef5f48c07815d36243d09abb0a52feb
coder-aastha <aasthadahal312@gmail.com>
2024-01-03 08:47:51 +0545

    Working on responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Navbar from './Navbar';
 import ImageSlider from "./ImageSlider";
 import Genres from "./Genres";
 import BestSellers from "./BestSellers";
 import NewArrivals from ".//NewArrivals";
 import Author from "./Author";
-import FooterUI from "./FooterUI";
-import AboutUs from "./AboutUs";
 
 export const Home = () => {
   return (
@@ -18,7 +15,6 @@ export const Home = () => {
       <BestSellers />
       <NewArrivals />
       <Author />
-      {/* <FooterUI /> */}
     </div>
   );
 };
