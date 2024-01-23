bcd9fc4ed3ab0340fb8c0d21c6fc8c602b00fa85
coder-aastha <aasthadahal312@gmail.com>
2024-01-23 08:00:52 +0545

    Certain changes on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import "./css/App.css";
 //  import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import { Home } from "./components/Home";
-import axios from 'axios';
-import {Routes, Route} from'react-router-dom';
-import {Toaster} from 'react-hot-toast'
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import UserLogin from "./components/UserLogin";
 import UserRegister from "./components/UserRegister";
-import BookDetail from './components/BookDetail'
+import BookDetail from "./components/BookDetail";
 import "../src/css/UserRegister.css";
 import "../src/css/UserLogin.css";
-import UserForgotPassword from './components/UserForgotPassword';
-import UserResetPassword from './components/UserResetPasspord';
+import UserForgotPassword from "./components/UserForgotPassword";
+import UserResetPassword from "./components/UserResetPasspord";
 
 import Categories from "./components/Categories";
 import AboutUs from "./components/AboutUs";
@@ -22,45 +22,41 @@ import Wishlist from "./components/Wishlist";
 import { AuthProvider } from "./components/AuthContext";
 import ShoppingCart from "./components/shoppingCart";
 // import { useState } from 'react';
-import AdminApp from './Admin/AdminApp';
-import AdminUploadBook from './Admin/AdminBookUpload'
-import AdminManageBook from './Admin/AdminManageBook'
-import AdminBookDetail from './Admin/AdminBookDetail'
+import AdminApp from "./Admin/AdminApp";
+import AdminUploadBook from "./Admin/AdminBookUpload";
+import AdminManageBook from "./Admin/AdminManageBook";
+import AdminBookDetail from "./Admin/AdminBookDetail";
 
-axios.defaults.baseURL ='http://localhost:3001';
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.withCredentials = true;
 
-const App =()=> {
-
+const App = () => {
   return (
     <>
-
-    <AuthProvider>
-      <Navbar />
-      <Toaster position='bottom-right' toastOptions={{duration:2000}} />
-    <Routes>
-    <Route path='/register' element={<UserRegister/>}/>
-    <Route path='/login' element={<UserLogin />}/>
-    <Route path='/' element={<Home />}/>
-    <Route path="/booklisting/:id" element={<BookDetail />} />
-    <Route path="/forgotpassword" element={<UserForgotPassword />}/>
-    <Route path='/resetpassword/:id' element={<UserResetPassword />} />
-    <Route path='/category/:category' element={<Categories />}/>
-    <Route path="/AboutUs" element={<AboutUs />} />
-    <Route path='/author/:authorName' element={<AuthorName />}/>
-    <Route path="/Wishlist" element={<Wishlist />}/>
-    <Route path="/shopping-cart" element={<ShoppingCart />} />
-    <Route path="/admin" element={<AdminApp/>}/>
-    <Route path="/upload-book" element={<AdminUploadBook/>}/>
-    <Route path="/AdminManageBook/:id" element={<AdminManageBook/>}/>
-    {/* <Route path="/manage-book/:id" element={<AdminManageBook/>}/> */}
-    <Route path="/booklisting" element={<AdminBookDetail/>}/>
-    </Routes>
-    <FooterUI/>
-    </AuthProvider>
-
+      <AuthProvider>
+        <Navbar />
+        <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
+        <Routes>
+          <Route path="/register" element={<UserRegister />} />
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/booklisting/:id" element={<BookDetail />} />
+          <Route path="/forgotpassword" element={<UserForgotPassword />} />
+          <Route path="/resetpassword/:id" element={<UserResetPassword />} />
+          <Route path="/category/:category" element={<Categories />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/author/:authorName" element={<AuthorName />} />
+          <Route path="/Wishlist" element={<Wishlist />} />
+          <Route path="/shopping-cart" element={<ShoppingCart />} />
+          <Route path="/admin" element={<AdminApp />} />
+          <Route path="/upload-book" element={<AdminUploadBook />} />
+          <Route path="/AdminManageBook/:id" element={<AdminManageBook />} />
+          <Route path="/booklisting" element={<AdminBookDetail />} />
+        </Routes>
+        <FooterUI />
+      </AuthProvider>
     </>
   );
-}
+};
 
 export default App;
