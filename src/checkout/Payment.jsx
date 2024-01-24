c46bb0b1c322313b2cfb029706bb690b3477dfc3
binu maka <binumaka9@gmail.com>
2024-01-24 09:49:12 +0545

    checkout update

--- src/checkout/Payment.jsx
+++ src/checkout/Payment.jsx
@@ -4,8 +4,10 @@ import Esewa from "../images/Esewa.png";
 import Khalti1 from "../images/Khalti1.png";
 import QR_code from "../images/QRcode.jpg";
 import QRcode from "../images/QRcode2.jpg";
+import { useNavigate } from "react-router-dom";
 
 function Payment() {
+  const navigate= useNavigate();
   const [selectedPaymentOption, setSelectedPaymentOption] = useState(() => {
     return localStorage.getItem('showPayment') === 'true';
   });
@@ -14,12 +16,24 @@ function Payment() {
     localStorage.setItem('selectedPaymentOption', selectedPaymentOption);
   }, [selectedPaymentOption]);
 
+  const handleContinue =()=>{
+    navigate("/")
+  }
+
   const handlePaymentOptionChange = (option) => {
     setSelectedPaymentOption(option);
   };
   return (
     <div className="paymentMain">
-       <h3>Choose any to pay:</h3>
+      <h2>Thank you for ordering!!</h2>
+      <div className="receipt_container">
+        <h4>Order details</h4>
+        <div className="receiptmain_container">
+        <p>Contact information</p>
+        <a>email</a>
+        </div>
+      </div>
+       <h4>Choose any to pay:</h4>
     <div className="payment-container">
      
       <div className="Payment_Method1" onClick={() => handlePaymentOptionChange('esewa')}>
@@ -42,6 +56,9 @@ function Payment() {
           <img src={QRcode} alt="QRcode2" />
         </div>
       )}
+      </div>
+      <div className="Continue" onClick={()=>handleContinue()}>
+        <p>Continue Shopping</p>
       </div>
     </div>
 
